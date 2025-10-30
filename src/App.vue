c0c52208f15b417329f20df87a832e7d80fa25f8
mattialeonetti <mattia.leonetti@uzh.ch>
2025-10-30 19:30:20 +0100

    Add PrimeVue to project Add example button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup></script>
-
 <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <Button label="PrimeVue Button" />
 </template>
 
+<script setup>
+import { Button } from 'primevue';
+</script>
 <style scoped></style>
