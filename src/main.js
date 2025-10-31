309a73897f4b28dcb6c161cbc12a7ee92244f613
mattialeonetti <mattia.leonetti@uzh.ch>
2025-10-31 11:03:07 +0100

    Add PrimeVue buttons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import 'primeicons/primeicons.css'
+import './assets/custom-theme.css'
 
 const app = createApp(App)
 app.use(createPinia())
